a122879f892742fc0a713ae804c36f720b309f8c
Richard Meadows <rich_meadows76@yahoo.com>
2020-03-10 20:38:27 -0700

    Lecture 167

---
+++ index.js
@@ -0,0 +1,8 @@
+
+var numberOfButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfButtons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+    alert("I definitely have it now!");
+  });
+}
