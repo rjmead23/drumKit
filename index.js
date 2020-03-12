c5cfde89ea34a3e06e7188c5b7d83e74b2c97fbd
Richard Meadows <rich_meadows76@yahoo.com>
2020-03-11 20:31:20 -0700

    sound for keydown and click

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+
+// Detecting button press
+
 var numberOfButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numberOfButtons; i++) {
@@ -5,63 +8,53 @@ for (var i = 0; i < numberOfButtons; i++) {
 
     var buttonInnerHTML = this.innerHTML;
 
-    switch (buttonInnerHTML) {
-      case "w":
-      var crash = new Audio('sounds/crash.mp3');
-      crash.play();
-        break;
+    makeSound(buttonInnerHTML);
 
-      case "a":
-      var kick = new Audio('sounds/kick-bass.mp3');
-      kick.play();
-      break;
-
-      case "s":
-      var snare = new Audio('sounds/snare.mp3');
-      snare.play();
-      break;
-
-      case "d":
-      var tom1 = new Audio('sounds/tom-1.mp3');
-      tom1.play();
-      break;
-
-      case "j":
-      var tom2 = new Audio('sounds/tom-2.mp3');
-      tom2.play();
-      break;
-
-      case "k":
-      var tom3 = new Audio('sounds/tom-3.mp3');
-      tom3.play();
-      break;
-
-      case "l":
-      var tom4 = new Audio('sounds/tom-4.mp3');
-      tom4.play();
-      break;
-
-      default: console.log(innerHTML);
-
-    }
   });
 }
 
-document.addEventListener("keydown", function(e){
-  console.log(e);
+// Detecting keydown
+document.addEventListener("keydown", function(event){
+  makeSound(event.key);
+});
 
-  switch (e.key) {
+function makeSound(key){
+  switch (key) {
     case "w":
     var crash = new Audio('sounds/crash.mp3');
     crash.play();
       break;
-    default:
 
+    case "a":
+    var kick = new Audio('sounds/kick-bass.mp3');
+    kick.play();
+    break;
+
+    case "s":
+    var snare = new Audio('sounds/snare.mp3');
+    snare.play();
+    break;
+
+    case "d":
+    var tom1 = new Audio('sounds/tom-1.mp3');
+    tom1.play();
+    break;
+
+    case "j":
+    var tom2 = new Audio('sounds/tom-2.mp3');
+    tom2.play();
+    break;
+
+    case "k":
+    var tom3 = new Audio('sounds/tom-3.mp3');
+    tom3.play();
+    break;
+
+    case "l":
+    var tom4 = new Audio('sounds/tom-4.mp3');
+    tom4.play();
+    break;
+
+    default: console.log("Key detected that doesn't have a sound: " + key);
   }
-
-
-
-
-
-
-});
+}
